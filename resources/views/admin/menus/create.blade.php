974dd19e19af1161163d40e8910dec8b6036c625
Nick <95492327+NickAcm21@users.noreply.github.com>
2023-06-16 16:11:27 +0630

    git push

--- resources/views/admin/menus/create.blade.php
+++ resources/views/admin/menus/create.blade.php
@@ -14,7 +14,7 @@
                 <form>
                     <div class="space-y-12">
                     <div class="border-b border-gray-900/10 pb-12">
-                        <h2 class="text-base font-semibold leading-7 text-gray-900">New Category</h2>
+                        <h2 class="text-base font-semibold leading-7 text-gray-900">News Category</h2>
                         <p class="mt-1 text-sm leading-6 text-gray-600">This new category will be displayed publicly so be careful what you create.</p>
 
                         <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
